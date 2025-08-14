58dde6c3a79c98be29c86aa206264df6b0477a51
Rebeca Azevedo <azevedorebeca9@gmail.com>
2025-08-14 20:22:55 -0300

    Feat: Adiciona componentes pendentes e ajusta setup

---
+++ src/components/common/product-item.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { productTable, productVariantTable } from "@/db/schema";
+import { formatCentsToBRL } from "@/helpers/money";
+import { cn } from "@/lib/utils";
+
+interface ProductItemProps {
+  product: typeof productTable.$inferSelect & {
+    variants: (typeof productVariantTable.$inferSelect)[];
+  };
+  textContainerClassName?: string;
+}
+
+const ProductItem = ({ product, textContainerClassName }: ProductItemProps) => {
+  const firstVariant = product.variants[0];
+  const raw = firstVariant.imageUrl;
+  const url = raw.replace(/[{}"]/g, "");
+
+  return (
+    <Link
+      href={`/product-variant/${firstVariant.slug}`}
+      className="flex flex-col gap-4"
+    >
+      <Image
+        src={url}
+        alt={firstVariant.name}
+        sizes="100vw"
+        height={0}
+        width={0}
+        className="h-auto w-full rounded-3xl"
+      />
+      <div
+        className={cn(
+          "flex max-w-[200px] flex-col gap-1",
+          textContainerClassName,
+        )}
+      >
+        <p className="truncate text-sm font-medium">{product.name}</p>
+        <p className="text-muted-foreground truncate text-xs font-medium">
+          {product.description}
+        </p>
+        <p className="truncate text-sm font-semibold">
+          {formatCentsToBRL(firstVariant.priceInCents)}
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default ProductItem;
